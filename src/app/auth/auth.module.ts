7acdca26848c09c6ab50ca0a19853f1a1f8e53a3
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2018-09-08 15:42:08 +0200

    CLI: ng g feature auth/Auth -m auth/auth.module.ts --group

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import * as fromAuth from './reducers/auth.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { AuthEffects } from './effects/auth.effects';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forFeature('auth', fromAuth.reducer),
+    EffectsModule.forFeature([AuthEffects])
   ],
   declarations: []
 })
