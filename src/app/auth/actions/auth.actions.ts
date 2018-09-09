db413bc19b5fe63392de283d372a9dab7d4f961e
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2018-09-09 15:35:53 +0200

    Add auth actions and reducer

--- src/app/auth/actions/auth.actions.ts
+++ src/app/auth/actions/auth.actions.ts
@@ -1,11 +1,33 @@
 import { Action } from '@ngrx/store';
 
 export enum AuthActionTypes {
-  LoadAuths = '[Auth] Load Auths'
+  LogIn = '[Auth] Log In',
+  AuthenticationFailed = '[Auth] Authentication Failed',
+  FacebookAuthenticated = '[Auth] Facebook Authenticated',
+  AuthenticateBackend = '[Auth] Authenticate Backend',
+  LoggedIn = '[Auth] Logged In'
 }
 
-export class LoadAuths implements Action {
-  readonly type = AuthActionTypes.LoadAuths;
+export class LogIn implements Action {
+  readonly type = AuthActionTypes.LogIn;
 }
 
-export type AuthActions = LoadAuths;
+export class AuthenticationFailed implements Action {
+  readonly type = AuthActionTypes.AuthenticationFailed;
+}
+
+export class FacebookAuthenticated implements Action {
+  readonly type = AuthActionTypes.FacebookAuthenticated;
+  constructor(public payload: any) { }
+}
+
+export class AuthenticateBackend implements Action {
+  readonly type = AuthActionTypes.AuthenticateBackend;
+}
+
+export class LoggedIn implements Action {
+  readonly type = AuthActionTypes.LoggedIn;
+  constructor(public payload: any) { }
+}
+
+export type AuthActions = LogIn | AuthenticationFailed | LoggedIn | FacebookAuthenticated | AuthenticateBackend;
--- src/app/auth/reducers/auth.reducer.ts
+++ src/app/auth/reducers/auth.reducer.ts
@@ -2,20 +2,60 @@ import { Action } from '@ngrx/store';
 import { AuthActions, AuthActionTypes } from '../actions/auth.actions';
 
 export interface State {
-
+  loggedIn: boolean;
+  loading: boolean;
+  facebookAccessToken: string;
+  backendAuthToken: string;
 }
 
 export const initialState: State = {
-
+  loggedIn: false,
+  loading: false,
+  facebookAccessToken: null,
+  backendAuthToken: null,
 };
 
 export function reducer(state = initialState, action: AuthActions): State {
   switch (action.type) {
-
-    case AuthActionTypes.LoadAuths:
-      return state;
-
-
+    case AuthActionTypes.LogIn: {
+      return {
+        ...state,
+        ...{
+          loading: true,
+          loggedIn: false
+        }
+      };
+    }
+    case AuthActionTypes.AuthenticationFailed: {
+      return {
+        ...state,
+        ...{
+          facebookAccessToken: null,
+          loading: false,
+          loggedIn: false
+        }
+      };
+    }
+    case AuthActionTypes.FacebookAuthenticated: {
+      return {
+        ...state,
+        ...{
+          facebookAccessToken: action.payload,
+          loading: true,
+          loggedIn: false
+        }
+      };
+    }
+    case AuthActionTypes.LoggedIn: {
+      return {
+        ...state,
+        ...{
+          backendAuthToken: action.payload,
+          loading: false,
+          loggedIn: true
+        }
+      };
+    }
     default:
       return state;
   }
