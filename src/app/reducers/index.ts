678ff459616b2f287440cbf0618662b637245e05
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2018-09-10 21:38:14 +0200

    Add store localstorage sync

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,6 +6,29 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { AuthActionTypes } from '../auth/actions/auth.actions';
+import { localStorageSync } from 'ngrx-store-localstorage';
+
+export function clearState(reducer: ActionReducer<any>): ActionReducer<any> {
+  return function (state, action) {
+    if (action.type === AuthActionTypes.LoggedOut) {
+      return reducer(undefined, action);
+    }
+    return reducer(state, action);
+  };
+}
+
+export function localStorageSyncReducer(
+  reducer: ActionReducer<any>
+): ActionReducer<any> {
+  return localStorageSync({ keys: ['auth'], rehydrate: true })(reducer);
+}
+
+export const metaReducers: Array<MetaReducer<any, any>> = [
+  localStorageSyncReducer,
+  clearState
+];
+
 
 export interface State {
 
@@ -14,6 +37,3 @@ export interface State {
 export const reducers: ActionReducerMap<State> = {
 
 };
-
-
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
