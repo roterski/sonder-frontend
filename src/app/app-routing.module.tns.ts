7b38581c101ed850520655a72ccd2c6d08b09586
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2018-09-12 18:38:03 +0200

    Move routes inside app-routing.module

--- src/app/app-routing.module.tns.ts
+++ src/app/app-routing.module.tns.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
-import { routes } from './app.routes';
+import { routes } from './app-routing.module';
 
 @NgModule({
   imports: [NativeScriptRouterModule.forRoot(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { routes } from './app.routes';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { authRoutes } from './modules/auth/auth-routing.module';
+import { AuthenticatedGuard } from './modules/auth/guards';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthenticatedGuard]
+  },
+  {
+    path: 'login',
+    children: authRoutes
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
