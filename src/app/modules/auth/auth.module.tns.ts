fb416d1edf17ee3e533206b10949fc13f2ac0969
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2018-09-13 18:09:33 +0200

    Move tns facebook initialization back to module

--- src/app/modules/auth/auth.module.tns.ts
+++ src/app/modules/auth/auth.module.tns.ts
@@ -8,6 +8,15 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './containers/login-page/login-page.component';
 import { NativeScriptHttpClientModule } from 'nativescript-angular/http-client';
 import { AuthenticatedGuard, UnauthenticatedGuard } from './guards';
+import { environment } from '../../../environments/environment';
+
+import * as application from 'application';
+
+const nsFacebook = require('nativescript-facebook');
+
+application.on(application.launchEvent, function (args) {
+    nsFacebook.init(environment.facebookAppId);
+});
 
 @NgModule({
     imports: [
--- src/app/modules/auth/services/auth.service.tns.ts
+++ src/app/modules/auth/services/auth.service.tns.ts
@@ -3,11 +3,7 @@ import * as Facebook from 'nativescript-facebook';
 import { NativeScriptHttpClientModule } from 'nativescript-angular/http-client';
 import { Observable, from, of } from 'rxjs';
 import { map, catchError, exhaustMap} from 'rxjs/operators';
-import { environment } from '../../../../environments/environment';
 import { bindNodeCallback } from 'rxjs/internal/observable/bindNodeCallback';
-import * as application from 'application';
-
-const nsFacebook = require('nativescript-facebook');
 
 @Injectable({
   providedIn: 'root'
@@ -17,9 +13,6 @@ export class AuthService implements OnInit {
   }
 
   ngOnInit() {
-    application.on(application.launchEvent, function (args) {
-      nsFacebook.init(environment.facebookAppId);
-    });
   }
 
   facebookLogIn(): Observable<any> {
